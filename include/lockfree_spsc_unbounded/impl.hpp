6e2db725821b5d147c4354adba7e3a648afca776
aryanchakravorty <mraryanchakravorty@gmail.com>
2026-03-19 01:10:06 +0530

    fix indentation

--- include/lockfree_spsc_unbounded/impl.hpp
+++ include/lockfree_spsc_unbounded/impl.hpp
@@ -1,116 +1,116 @@
-    #ifndef LOCKFREE_SPSC_UNBOUNDED_IMPL
-    #define LOCKFREE_SPSC_UNBOUNDED_IMPL
+#ifndef LOCKFREE_SPSC_UNBOUNDED_IMPL
+#define LOCKFREE_SPSC_UNBOUNDED_IMPL
 
-    #include "defs.hpp"
+#include "defs.hpp"
 
 
-    template <typename T>
-    using queue = typename tsfqueue::__impl::lockfree_spsc_unbounded<T>;
- 
-    template <typename T> void queue<T>::push(T value) {
+template <typename T>
+using queue = typename tsfqueue::__impl::lockfree_spsc_unbounded<T>;
 
-        static_assert(std::is_move_constructible_v<T>, 
-            "T must be move constructible");
+template <typename T> void queue<T>::push(T value) {
 
-        emplace_back(std::move(value));
-    }
+    static_assert(std::is_move_constructible_v<T>, 
+        "T must be move constructible");
 
-    template <typename T> bool queue<T>::try_pop(T &value) {  
+    emplace_back(std::move(value));
+}
 
-        static_assert(std::is_nothrow_destructible_v<T>,
-                  "T must be nothrow destructible");  
-        static_assert(std::is_move_assignable_v<T>,
-                  "T must be move-assignable");
+template <typename T> bool queue<T>::try_pop(T &value) {  
 
-        node* new_head = head->next.load(std::memory_order_acquire);
-
-        if(new_head == nullptr){
-            return false;
-        }
-
-        node* old_head = head;
-        value = std::move(head->data);
-        head = new_head;
-
-        delete old_head;
-
-        capacity.fetch_sub(1,std::memory_order_relaxed);
-
-        return true;
-    }   
-
-    template <typename T> void queue<T>::wait_and_pop(T &value) {
-
-        static_assert(std::is_nothrow_destructible_v<T>,
-            "T must be nothrow destructible"); 
-        static_assert(std::is_move_assignable_v<T>,
-            "T must be move-assignable"); 
-            
-        node * new_head;
-        while( ( new_head = head->next.load(std::memory_order_acquire) ) == nullptr ){
-            std::this_thread::yield();//low latency-high cpu usage
-        }
-
-        node* old_head = head;
-        value = std::move(head->data);
-        head = new_head;
-
-        delete old_head;
-
-        capacity.fetch_sub(1,std::memory_order_relaxed);
-        
-    }
-
-    template <typename T> bool queue<T>::peek(T &value) {
-
-        static_assert(std::is_copy_assignable_v<T>, 
-                "T must be copy-assignable");
-
-        if(empty()){
-            return false;
-        }
-
-        value = head -> data;
-        return true;
-    }
-
-    template <typename T> bool queue<T>::empty(void) {
-        return (head->next.load(std::memory_order_acquire) == nullptr);
-    }
-
-    template<typename T>
-    template<typename... Args>
-    void queue<T>::emplace_back(Args&&... args)
-    {       
-            static_assert(std::is_constructible_v<T,Args&&...>,
-                "T must be constructible with Args&&...");
-            static_assert(std::is_default_constructible_v<T>,
-                "T must be default constructible");
-            static_assert(std::is_move_assignable_v<T>,
+    static_assert(std::is_nothrow_destructible_v<T>,
+                "T must be nothrow destructible");  
+    static_assert(std::is_move_assignable_v<T>,
                 "T must be move-assignable");
 
-            node* stub = new node();
-            stub->next.store(nullptr,std::memory_order_relaxed);
+    node* new_head = head->next.load(std::memory_order_acquire);
 
-            tail->data = T(std::forward<Args>(args)...);
-            capacity.fetch_add(1,std::memory_order_relaxed);
-            tail->next.store(stub,std::memory_order_release);
-
-            tail = stub;
+    if(new_head == nullptr){
+        return false;
     }
 
-    template<typename T>
-    size_t queue<T>::size(){
-        return capacity.load(std::memory_order_relaxed);
+    node* old_head = head;
+    value = std::move(head->data);
+    head = new_head;
+
+    delete old_head;
+
+    capacity.fetch_sub(1,std::memory_order_relaxed);
+
+    return true;
+}   
+
+template <typename T> void queue<T>::wait_and_pop(T &value) {
+
+    static_assert(std::is_nothrow_destructible_v<T>,
+        "T must be nothrow destructible"); 
+    static_assert(std::is_move_assignable_v<T>,
+        "T must be move-assignable"); 
+        
+    node * new_head;
+    while( ( new_head = head->next.load(std::memory_order_acquire) ) == nullptr ){
+        std::this_thread::yield();//low latency-high cpu usage
     }
 
-    #endif
+    node* old_head = head;
+    value = std::move(head->data);
+    head = new_head;
 
-    // 1. Add static asserts
+    delete old_head;
 
-    // 2. Add emplace_back using perfect forwarding and variadic templates (you
-    // can use this in push then)
+    capacity.fetch_sub(1,std::memory_order_relaxed);
+    
+}
 
-    // 3. Add size() function
+template <typename T> bool queue<T>::peek(T &value) {
 
-    // 4. Any more suggestions ??
+    static_assert(std::is_copy_assignable_v<T>, 
+            "T must be copy-assignable");
+
+    if(empty()){
+        return false;
+    }
+
+    value = head -> data;
+    return true;
+}
+
+template <typename T> bool queue<T>::empty(void) {
+    return (head->next.load(std::memory_order_acquire) == nullptr);
+}
+
+template<typename T>
+template<typename... Args>
+void queue<T>::emplace_back(Args&&... args)
+{       
+        static_assert(std::is_constructible_v<T,Args&&...>,
+            "T must be constructible with Args&&...");
+        static_assert(std::is_default_constructible_v<T>,
+            "T must be default constructible");
+        static_assert(std::is_move_assignable_v<T>,
+            "T must be move-assignable");
+
+        node* stub = new node();
+        stub->next.store(nullptr,std::memory_order_relaxed);
+
+        tail->data = T(std::forward<Args>(args)...);
+        capacity.fetch_add(1,std::memory_order_relaxed);
+        tail->next.store(stub,std::memory_order_release);
+
+        tail = stub;
+}
+
+template<typename T>
+size_t queue<T>::size(){
+    return capacity.load(std::memory_order_relaxed);
+}
+
+#endif
+
+// 1. Add static asserts
+
+// 2. Add emplace_back using perfect forwarding and variadic templates (you
+// can use this in push then)
+
+// 3. Add size() function
+
+// 4. Any more suggestions ??
