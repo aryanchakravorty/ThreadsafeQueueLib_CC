d262bac152fe18c2751b1007af755be52db2afbc
Coder-124421 <tushar3.0x@gmail.com>
2026-03-19 23:27:25 +0530

    Refactor lockfree_spsc_unbounded implementation: streamline push, try_pop, and wait_and_pop methods; replace capacity with size; enhance static assertions.

--- include/lockfree_spsc_unbounded/defs.hpp
+++ include/lockfree_spsc_unbounded/defs.hpp
@@ -5,135 +5,78 @@
 #include <atomic>
 #include <memory>
 #include <type_traits>
-#include <thread>
-#include <cstddef>
 
+namespace tsfqueue::__impl
+{
+  template <typename T>
+  class lockfree_spsc_unbounded
+  {
+    // Works exactly same as the blocking_mpmc_unbounded queue (see this once)
+    // with tail pointer pointing to stub node and your head pointer updates as
+    // per the pushes. See the Lockless_Node in utils to understand the working.
+    // Note that the next pointers are atomic there. Why ?? [Reason this]
+    // Also the head and tail members are cache-aligned. Why ?? [Reason this] (ask
+    // me for details)
 
-namespace tsfqueue::__impl {
-template <typename T> class lockfree_spsc_unbounded {
-  // Works exactly same as the blocking_mpmc_unbounded queue (see this once)
-  // with tail pointer pointing to stub node and your head pointer updates as
-  // per the pushes. See the Lockless_Node in utils to understand the working.
+    // [Copy of blocking_mpmc_unbounded]
+    // For the implementation, we start with a stub node and both head and tail
+    // are initialized to it. When we push, we make a new stub node, move the data
+    // into the current tail and then change the tail to the new stub. We have two
+    // methods : wait_and_pop() which waits on the queue and returns element &
+    // try_pop() which returns an element if queue is not empty otherwise returns
+    // some neutral element OR a false boolean whichever is applicable. Pop works
+    // by returning the data stored in head node and replacing head to its next
+    // node. We handle the empty queue gracefully as per the pop type.
+  private:
+    using node = tsfqueue::__utils::Lockless_Node<T>;
 
-  //--------------------------------------------------------------------------
-  // Note that the next pointers are atomic there. Why ?? [Reason this]
-  //
-  //if head and tail point to same stub node ,producer is trying to modify next ptr,consumer is trying to read
-  // next ptr(to check if queue is empty)->race condition
-  //--------------------------------------------------------------------------
-
-  //------------------------------------------------------------------------------
-  // Also the head and tail members are cache-aligned. Why ?? [Reason this] (ask me for details)
-  //
-  //cpu update on common cache line,if producer makes some changes,it invalidates other cores(consumers) cache,so this has to keep on updating
-  //which is time consuming
-  //cache aligning make the pointers sit on different cache lines,so now we can spam modifications
-  //--------------------------------------------------------------------------------
-
-  // [Copy of blocking_mpmc_unbounded]
-  // For the implementation, we start with a stub node and both head and tail
-  // are initialized to it. 
-
-  //When we push, we make a new stub node, move the data
-  // into the current tail and then change the tail to the new stub. 
-
-  //We have two methods : wait_and_pop() which waits on the queue and returns element &
-  // try_pop() which returns an element if queue is not empty otherwise returns
-  // some neutral element OR a false boolean whichever is applicable. Pop works
-  // by returning the data stored in head node and replacing head to its next
-  // node. We handle the empty queue gracefully as per the pop type.
-
-private:
-  using node = tsfqueue::__utils::Lockless_Node<T>;
-
-  // Add the private members :
-  // 1. node* head;
-  // 2. node* tail;
-
-  alignas(tsfq::__impl::cache_line_size)node * head;
-  alignas(tsfq::__impl::cache_line_size)node *tail;
-
-  // Description of private members :
-  // 1. node* head -> Pointer to the head node
-  // 2. node* tail -> Pointer to tail node
-  // 3. Cache align 1-2
-
-  //capacity 
-  alignas(tsfq::__impl::cache_line_size)std::atomic<size_t>capacity {0};
-
-public:
-  // Public member functions :
-
-
-  // Add relevant constructors and destructors -> Add these here only
-
-  lockfree_spsc_unbounded(){
-
-    static_assert(std::is_default_constructible_v<T>,
-        "T must be default constructible");
-
-    head = new node();
-    head->next.store(nullptr,std::memory_order_relaxed);
-
-    tail = head;
-
-  }
-
-  ~lockfree_spsc_unbounded(){
-
-    static_assert(std::is_nothrow_destructible_v<T>,
-          "T must be nothrow destructible");  
-
-    while(head != nullptr){
-      node* current = head;
-      head = head->next.load(std::memory_order_relaxed);
-      delete current;
+    // Add the private members :
+    // 1. node* head;
+    // 2. node* tail;
+    node *head;
+    node *tail;
+    std::atomic<size_t>sz{0};
+    // Description of private members :
+    // 1. node* head -> Pointer to the head node
+    // 2. node* tail -> Pointer to tail node
+    // 3. Cache align 1-2
+    
+    public:
+    // Public member functions :
+    // Add relevant constructors and destructors -> Add these here only
+    lockfree_spsc_unbounded(){
+      node* stub = new node;
+      stub->next.store(nullptr,std::memory_order_relaxed);
+      head = tail = stub;
     }
-  }
-
-  //Delete Copy and Move constructor
-  //Copy constructor not required as its spsc,could also cause double free crash
-  //move constructor can break the queue if thread is running and we move the queue somewhere else
-
-  lockfree_spsc_unbounded(const lockfree_spsc_unbounded&)=delete;
-  lockfree_spsc_unbounded &operator=(const lockfree_spsc_unbounded &)=delete;
-
-  lockfree_spsc_unbounded(lockfree_spsc_unbounded&&) = delete;
-  lockfree_spsc_unbounded &operator=(lockfree_spsc_unbounded&&) = delete;
-
-  // 1. void push(value) : Pushes the value inside the queue, copies the value
-  void push(T);
-
-  // 2. void wait_and_pop(value ref) : Blocking wait on queue, returns value in
-  // the reference passed as parameter
-  void wait_and_pop(T&);
-
-  // 3. bool try_pop(value ref) : Returns true and
-  // gives the value in reference passed, false otherwise
-  bool try_pop(T&);
-
-  // 4. bool empty() : Returns
-  // whether the queue is empty or not at that instant
-  bool empty();
-
-  // 5. bool peek(value ref) : Returns the front/top element of queue in ref (false if empty queue)
-  bool peek(T&);
-
-  // 6. Add static asserts
-
-  // 7. Add emplace_back using perfect forwarding and variadic templates (you
-  // can use this in push then)
-  template<typename... Args>
-  void emplace_back(Args&&... args);
-
-  // 8. Add size() function
-  size_t size();
-
-  // 9. Any more suggestions ??
-
-  // 10. Why no shared_ptr ?? [Reason this]
-  //
-};
+    ~lockfree_spsc_unbounded(){      node* curr = head;
+      while(curr!=nullptr){
+        node* next = curr->next.load(std::memory_order_relaxed);
+        delete curr;
+        curr = next;
+       }
+      }
+    // 1. void push(value) : Pushes the value inside the queue, copies the value
+    void push(T);
+      // 2. void wait_and_pop(value ref) : Blocking wait on queue, returns value in
+      // the reference passed as parameter
+      void wait_and_pop(T&);
+      // 3. bool try_pop(value ref) : Returns true and
+      // gives the value in reference passed, false otherwise
+      bool try_pop(T& value);
+    // 4. bool empty() : Returns
+    // whether the queue is empty or not at that instant
+    bool empty(void);
+    // 5. bool peek(value ref) : Returns the front/top element of queue in ref (false if empty queue)
+    bool peek(T& value);
+    // 6. Add static asserts
+    // 7. Add emplace_back using perfect forwarding and variadic templates (you
+    // can use this in push then)
+    // 8. Add size() function
+    size_t size();
+    // 9. Any more suggestions ??
+    // 10. Why no shared_ptr ?? [Reason this]
+  };
 } // namespace tsfqueue::__impl
 
 #endif
--- include/lockfree_spsc_unbounded/impl.hpp
+++ include/lockfree_spsc_unbounded/impl.hpp
@@ -3,114 +3,66 @@
 
 #include "defs.hpp"
 
-
 template <typename T>
 using queue = tsfqueue::__impl::lockfree_spsc_unbounded<T>;
 
 template <typename T> void queue<T>::push(T value) {
-
-    static_assert(std::is_move_constructible_v<T>, 
-        "T must be move constructible");
-
-    emplace_back(std::move(value));
+     node* new_stub  = new node;
+        new_stub->next.store(nullptr,std::memory_order_relaxed);
+        tail->data = std::move(value);
+        sz.fetch_add(1,std::memory_order_relaxed);
+        tail->next.store(new_stub,std::memory_order_release);
+        tail = new_stub;
 }
 
-template <typename T> bool queue<T>::try_pop(T &value) {  
-
-    static_assert(std::is_nothrow_destructible_v<T>,
-                "T must be nothrow destructible");  
-    static_assert(std::is_move_assignable_v<T>,
-                "T must be move-assignable");
-
-    node* new_head = head->next.load(std::memory_order_acquire);
-
-    if(new_head == nullptr){
-        return false;
-    }
-
-    node* old_head = head;
-    value = std::move(head->data);
-    head = new_head;
-
-    delete old_head;
-
-    capacity.fetch_sub(1,std::memory_order_relaxed);
-
-    return true;
-}   
+template <typename T> bool queue<T>::try_pop(T &value) {
+node* next = head->next.load(std::memory_order_acquire);
+        static_assert(std::is_move_assignable<T>::value,
+                  "T must be move assignable for try_pop to work\n");
+        static_assert(std::is_nothrow_move_assignable<T>::value,"T's move assignment  must not throw exceptions!");
+        if(next==nullptr){
+          return false;
+        }      
+        else {
+          value = std::move(head->data);
+          node* old = head;
+          head = next;
+          delete old;
+          sz.fetch_sub(1,std::memory_order_relaxed);
+          return true;
+      }
+}
 
 template <typename T> void queue<T>::wait_and_pop(T &value) {
-
-    static_assert(std::is_nothrow_destructible_v<T>,
-        "T must be nothrow destructible"); 
-    static_assert(std::is_move_assignable_v<T>,
-        "T must be move-assignable"); 
-        
-    node * new_head;
-    while( ( new_head = head->next.load(std::memory_order_acquire) ) == nullptr ){
-        std::this_thread::yield();//low latency-high cpu usage
-    }
-
-    node* old_head = head;
-    value = std::move(head->data);
-    head = new_head;
-
-    delete old_head;
-
-    capacity.fetch_sub(1,std::memory_order_relaxed);
-    
+    while((head->next.load(std::memory_order_acquire))==nullptr) continue;
+        node* next = head->next.load(std::memory_order_acquire);
+        sz.fetch_sub(1,std::memory_order_relaxed);
+        value = std::move(head->data);
+        node* old = head;
+        head = next;
+        delete old;
 }
 
 template <typename T> bool queue<T>::peek(T &value) {
-
-    static_assert(std::is_copy_assignable_v<T>, 
-            "T must be copy-assignable");
-
-    if(empty()){
-        return false;
-    }
-
-    value = head -> data;
-    return true;
+    if(empty()) return false;
+      else {
+        value = head->data;
+        return true;
+      }
 }
 
 template <typename T> bool queue<T>::empty(void) {
-    return (head->next.load(std::memory_order_acquire) == nullptr);
+    return head->next.load(std::memory_order_acquire)==nullptr;
 }
 
-template<typename T>
-template<typename... Args>
-void queue<T>::emplace_back(Args&&... args)
-{       
-        static_assert(std::is_constructible_v<T,Args&&...>,
-            "T must be constructible with Args&&...");
-        static_assert(std::is_default_constructible_v<T>,
-            "T must be default constructible");
-        static_assert(std::is_move_assignable_v<T>,
-            "T must be move-assignable");
-
-        node* stub = new node();
-        stub->next.store(nullptr,std::memory_order_relaxed);
-
-        tail->data = T(std::forward<Args>(args)...);
-        capacity.fetch_add(1,std::memory_order_relaxed);
-        tail->next.store(stub,std::memory_order_release);
-
-        tail = stub;
-}
-
-template<typename T>
-size_t queue<T>::size(){
-    return capacity.load(std::memory_order_relaxed);
+template <typename T> size_t size(void){
+    return sz.load(std::memory_order_relaxed);
 }
 
 #endif
 
 // 1. Add static asserts
-
 // 2. Add emplace_back using perfect forwarding and variadic templates (you
 // can use this in push then)
-
 // 3. Add size() function
-
 // 4. Any more suggestions ??
